203c23a2de46e5b01f0519087963ac385bcc9d3c
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-20 20:32:25 +0530

    checkattendance

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace companywage213
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to company employye wage");
+
+
+            employeecheck.checkattendance();
+            
+        }
+    }
+} 
+        
