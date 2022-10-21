057a3959f610a062036a70e6d9ee36c26d4c3e0f
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-21 20:10:27 +0530

    check empwage

---
+++ companyempwage.cs
@@ -0,0 +1,34 @@
+﻿namespace companywage213
+{
+
+    public interface icompanyempwage
+    {
+        public void addcompanyempwage(string company, int emprateperhour, int numofworkingdays, int maxhourspermonth);
+        public void computeempwage();
+        public int gettotalwage(int company);
+    }
+        public class companyempwage
+        {
+            public string company;
+            public int emprateperhour;
+            public int numofworkingdays;
+            public int maxhourspermonth;
+            public int totalempwage;
+
+            public companyempwage(string company, int emprateperhour, int numofworkingdays, int maxhourspermonth)
+            {
+                this.company = company;
+                this.emprateperhour = emprateperhour;
+                this.numofworkingdays = numofworkingdays;
+                this.maxhourspermonth = maxhourspermonth;
+            }
+            public void totalwage()
+            {
+                this.totalempwage = totalempwage;
+            }
+            public string tostring()
+            {
+                return "total emp wage for company:" + this.company + "is:" + this.totalempwage;
+            }
+        }
+}
