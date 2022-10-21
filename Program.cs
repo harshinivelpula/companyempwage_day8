fc847e155bc7729211e080d3564958dfe74e376b
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-21 14:25:32 +0530

    multipleempwage

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ namespace companywage213
             wageformonth.monthwages();
             condition.conditions();
             computeempwage.calcomputewage();
+            multiplecompany.multipleempwage("dmart", 20, 2, 10);
+            multiplecompany.multipleempwage("reliance", 10, 4, 20);
 
         }
     }
